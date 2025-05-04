e096705f257a23d6f72eb9e2dfd4439f26ebff68
fahminlb33 <fahminlb33@gmail.com>
2025-05-04 08:17:23 +0700

    fix: add network and path

--- tools/run-local-pdp.sh
+++ tools/run-local-pdp.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 docker run -d -it \
   -p 7766:7000 \
-  --env-file .env \
+  --env-file ../.env \
+  --net devto-permitio-mcp \
   --name devto-permitio-mcp-pdp \
   permitio/pdp-v2:latest
